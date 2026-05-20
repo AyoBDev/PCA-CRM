c15bd09783c0273cf451099b23a5b667284174e0
AyoBDev <ayobamitunmise99@gmail.com>
2026-05-20 23:23:17 +0100

    fix: purge orphaned document records with no file data

--- server/prisma/migrations/20260520000000_add_file_data_to_documents/migration.sql
+++ server/prisma/migrations/20260520000000_add_file_data_to_documents/migration.sql
@@ -3,3 +3,7 @@ ALTER TABLE "authorization_documents" ADD COLUMN "file_data" BYTEA;
 
 -- AlterTable
 ALTER TABLE "client_documents" ADD COLUMN "file_data" BYTEA;
+
+-- Purge orphaned document records that have no file data (files lost during prior deploys)
+DELETE FROM "authorization_documents" WHERE "file_data" IS NULL;
+DELETE FROM "client_documents" WHERE "file_data" IS NULL;
