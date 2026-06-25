08d5c24f248aa51231fcea3c3bfed4d10a45e0fd
AyoBDev <ayobamitunmise99@gmail.com>
2026-06-25 18:58:24 +0100

    feat(employee-app): add CERT_TYPES constant

---
+++ employee-app/src/utils/certTypes.js
@@ -0,0 +1,10 @@
+export const CERT_TYPES = Object.freeze([
+  'TB Test',
+  'CPR',
+  'Annual Training',
+  'Cultural Competency',
+  'Infection Control',
+  'Background Check',
+  'ID',
+  'Other',
+]);
